634e58ea6cdd37410ad2bd91fb13fa20a606c14d
Nanda <107196595+Nandaaaskz@users.noreply.github.com>
2024-10-18 10:49:36 -0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,13 +1,12 @@
-import { aleatorio } from "./aleatorio";
-import { perguntas } from "./perguntas";
+import { aleatorio } from "./aleatorio.js";
+import { perguntas } from "./perguntas.js";
 
-const caixaPrincipal = document.querySelector(".caixa-principal")
-const caixaPerguntas = document.querySelector(".caixa-perguntas")
-const caixaAlternativas = document.querySelector(".caixa-alternativas")
-const caixaResultado = document.querySelector(".caixa-resultado")
-const textoResultado = document.querySelector(".texto-resultado")
-const botaoJogarNovamente = document.querySelector(".novamente-btn")
-
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+const botaoJogarNovamente = document.querySelector(".novamente-btn");
 
 
 let atual = 0; 
@@ -36,7 +35,7 @@ function mostraAlternativas(){
 }
 
 function respostaSelecionada(opcaoSelecionada){
-    const afirmacoes = opcaoSelecionada.afirmacao;
+    const afirmacoes = aleatorio(opcaoSelecionada.afirmacao);
     historiaFinal += afirmacoes + "";
     atual++;
     mostraPergunta();
@@ -55,11 +54,5 @@ function jogarNovamente(){
     mostraPergunta();
 }
 
+
 mostraPergunta();
-
-
-
-
-
-
-aleatorio
