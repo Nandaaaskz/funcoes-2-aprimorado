9d8c8f8485a143c35f1248963d9f3b654b731a2f
Nanda <107196595+Nandaaaskz@users.noreply.github.com>
2024-11-08 09:44:55 -0300

    Add files via upload

--- js/aleatorio.js
+++ js/aleatorio.js
@@ -1,4 +1,13 @@
+const nomes = ["Nanda", "Nana", "Lulu", "Rico"]
+
+
+
+
+
+
 export function aleatorio(lista){
         const posicao = Math.floor(Math.random()*lista.length);
         return lista[posicao];
 }
+
+export const nome = aleatorio(nomes);
